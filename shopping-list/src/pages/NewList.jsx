e9232da5839a4d89b6ed87f8dce4202785f26a53
Breno Almeida <108958237+brenoealmeida@users.noreply.github.com>
2023-07-20 16:09:09 -0300

    dev: begins to develop NewLists page [not working]

--- shopping-list/src/pages/NewList.jsx
+++ shopping-list/src/pages/NewList.jsx
@@ -1,17 +1,53 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Context from "../context/Context";
+import Form from '../components/Form';
+import Input from '../components/Input';
+import Button from "../components/Button";
 
 function NewList() {
-    const { token } = useContext(Context)
+    const { token } = useContext(Context);
+    const [inputs, setInputs] = useState([0]);
 
     useEffect(() => {
         console.log(token);
     }, [])
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const handleChange = () => {
+        
+    }
+
+    const handleClick = () => {
+        setInputs((prev) => {
+            const num = prev.length;
+            return {
+                ...prev,
+                num
+            }
+        })
+    }
+
     return (
-        <div>
-            Página para nova lista
-        </div>
+        <section>
+            <h2>Criar nova lista de compras:</h2>
+            <Form onSubmit={handleSubmit}>
+                {
+                    inputs.map((index) => (
+                        <Input placeholder='Digite o ID do projeto'
+                            type='number'
+                            name={`id_${index}`}
+                            key={index}
+                            onChange={handleChange}
+                            />
+                    ))
+                }
+                <Button type="button" onClick={handleClick}>Adicionar projeto à lista</Button>
+                <Button type="submit">Enviar</Button>
+            </Form>
+        </section>
     )
 }
 
