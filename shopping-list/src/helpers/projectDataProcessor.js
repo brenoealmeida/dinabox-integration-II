87887dd50a10540d0c8efbe117c0eff8e2fad9b6
Breno Almeida <brenoedalmeida@gmail.com>
2023-07-22 14:30:16 -0300

    dev: continues to develop data processor

--- shopping-list/src/helpers/projectDataProcessor.js
+++ shopping-list/src/helpers/projectDataProcessor.js
@@ -1,5 +1,38 @@
-function dataToShoppingList (data) {
-  console.log(data);
+const categories = [
+  'Ferragens e acessórios',
+  'Fitas',
+  'Ferragens BLUM',
+  'Terceirizados',
+  'Vidraçaria',
+  'Rafix',
+  'Porta de Alumínio',
+  'Perfis e Puxadores',
+  'Trilhos'
+]
+
+function modulesProcessor(modules) {
+  const { parts, inputs } = modules;
+
+
 }
+
+function jointSystemProcessor(data) {
+  const result = data.map((component) => {
+    
+  })
+}
+
+
+function dataToShoppingList (data) {
+  const jointSystem = data.holes;
+  const modules = data.woodwork;
+  const extraComponents = data.components.data;
+  const project_name = data.project_description;
+  const customer_name = data.project_customer_name;
+
+
+}
+
+
 
 export default dataToShoppingList;
